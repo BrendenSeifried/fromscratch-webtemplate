00df2762f702eedd8854a553643ef967b2cccd12
BrendenSeifried <brendenseifried@gmail.com>
2022-02-10 17:05:32 -0800

    trying second test

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -1,9 +1,9 @@
 // IMPORT MODULES under test here:
-import { renderPlaneDetail } from '../render-utils.js';
+import { renderPlaneDetail, renderPlaneData } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('renderPlaneData test', (expect) => {
+test('renderPlaneDetail test', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = `<div class="plane-detail"><h2 class="type">Jumbo Jet.</h2><img src="../assets/1.png"><p class="speed">Medium speed intercontinental distance.</p><p class="size">Large.</p></div>`;
@@ -15,6 +15,27 @@ test('renderPlaneData test', (expect) => {
         type: 'Jumbo Jet.',
         size: 'Large.',
         speed: 'Medium speed intercontinental distance.',
+        
+
+    });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual.outerHTML, expected);
+});
+
+
+test('renderPlaneData test', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<div class="plane-detail"><h2 class="type">Jumbo Jet.</h2><img src="../assets/1.png"><p class="speed">Medium speed intercontinental distance.</p><p class="size">Large.</p></div>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderPlaneData({
+        p: 'Jumbo Jet.'
+        a: './planesfolder/?id=1',
+       
         
 
     });
